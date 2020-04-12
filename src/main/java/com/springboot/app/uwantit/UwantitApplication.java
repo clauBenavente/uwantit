6ff2f421a2111310b2625507f8c7e4b0bd81c19b
clauBenavente <claumn94@gmail.com>
2020-04-12 17:52:00 +0200

    Subida del proyecto vacio

---
+++ src/main/java/com/springboot/app/uwantit/UwantitApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.uwantit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwantitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UwantitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/uwantit/UwantitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.uwantit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwantitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
