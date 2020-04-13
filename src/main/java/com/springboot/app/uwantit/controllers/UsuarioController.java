026ccc6b5950f83b57f27408e349f0036a2264d7
Joseldcg <josepitu747@gmail.com>
2020-04-13 13:46:24 +0200

    Clases Usuario y UsuarioController hechas, creadas carpetas static y templates. Añadidas vistas formulario registro y login a templates

--- src/main/java/com/springboot/app/uwantit/controllers/UsuarioController.java
+++ src/main/java/com/springboot/app/uwantit/controllers/UsuarioController.java
@@ -1,5 +1,47 @@
 package com.springboot.app.uwantit.controllers;
 
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.springboot.app.uwantit.models.entity.Usuario;
+
+
+@Controller
 public class UsuarioController {
 
+	//@Autowired
+	//private UsuarioServiceImpl service;
+	
+	@GetMapping(value="/form")
+	public String formularioRegitro(Model model) {
+		model.addAttribute("titulo", "Registro");
+		model.addAttribute("usuario", new Usuario());
+		return "formularioRegistro";
+	}
+	
+	@PostMapping(value="/form")
+	public String procesarRegistro(Usuario usuario) {
+		//service.insertarUsuario(usuario);
+		return "redirect:/index";
+	}
+	
+	@GetMapping(value="/login")
+	public String formularioLogin(Model model) {
+		model.addAttribute("titulo", "Login");
+		return "formularioLogin";
+	}
+	
+	@PostMapping(value="/login")
+	public String confirmarLogin() {
+		//service.confirmarUsuario(usuario);
+		return "redirect:/index";
+	}
+	
+	/*public void puntuarUsuario(int puntuacion) {
+		
+	}*/
+	
 }
--- src/main/java/com/springboot/app/uwantit/models/entity/Usuario.java
+++ src/main/java/com/springboot/app/uwantit/models/entity/Usuario.java
@@ -1,5 +1,109 @@
 package com.springboot.app.uwantit.models.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity(name = "Usuarios")
 public class Usuario {
+	@NotBlank
+	private String login;
+	@NotBlank
+	private String nombre;
+	@NotBlank
+	private String apellidos;
+	@NotBlank
+	private String contrasenna;
+	@NotNull
+    @Pattern(regexp = "[6]{1}[0-9]{8}", message="El número indicado no es un número de telefono")
+	private int telefono;
+	@Id
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String direccion;
+	@NotBlank
+	private String fotoPerfil;
+	private int rolUsuario;
+
+	public Usuario() {
+
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getContrasenna() {
+		return contrasenna;
+	}
+
+	public void setContrasenna(String contrasenna) {
+		this.contrasenna = contrasenna;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getFotoPerfil() {
+		return fotoPerfil;
+	}
+
+	public void setFotoPerfil(String fotoPerfil) {
+		this.fotoPerfil = fotoPerfil;
+	}
+
+	public int getRolUsuario() {
+		return rolUsuario;
+	}
+
+	public void setRolUsuario(int rolUsuario) {
+		this.rolUsuario = rolUsuario;
+	}
 
 }
