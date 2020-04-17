ac7b66f7bf1e3444d8c722851a10d3e077752a0c
laki <laki@192.168.1.157>
2020-04-17 17:10:20 +0200

    añadido metodo de eliminar Producto

--- src/main/java/com/springboot/app/uwantit/controllers/ProductoController.java
+++ src/main/java/com/springboot/app/uwantit/controllers/ProductoController.java
@@ -18,56 +18,64 @@ import com.springboot.app.uwantit.models.service.IProductoService;
 
 @Controller
 public class ProductoController {
-	
+
 	@Autowired
 	private IProductoService productoService;
-	
-	@RequestMapping(value="/listar")
+
+	@RequestMapping(value = "/listar")
 	public String listarTodosLosProductos(Model model) {
 		model.addAttribute("titulo", "Listado de Productos");
 		model.addAttribute("productos", productoService.listarProductos());
 		return "listar";
 	}
 
-	@GetMapping(value="/formularioProducto")
+	@GetMapping(value = "/formularioProducto")
 	public String formularioProducto(Model model) {
 		model.addAttribute("titulo", "Registro de Producto");
 		model.addAttribute("producto", new Producto());
 		return "formularioProducto";
 	}
 
-	  @PostMapping(value="/formProducto")
+	@PostMapping(value = "/formProducto")
 	public String procesarProducto(@Valid Producto producto, BindingResult result, Model model) {
-		/*if(result.hasErrors()){
-			model.addAttribute("titulo", "Registro de Producto");
-			
-			return "formularioProducto";
-		}*/
+		/*
+		 * if(result.hasErrors()){ model.addAttribute("titulo", "Registro de Producto");
+		 * 
+		 * return "formularioProducto"; }
+		 */
 		productoService.insertarProducto(producto);
 		return "redirect:/listar";
 	}
-	 
-	
-	
-	@RequestMapping(value="/producto/{idProducto}")
-	public String visualizarProducto(@PathVariable(value="idProducto") Long idProducto, Map<String, Object> model) {
+
+	@RequestMapping(value = "/producto/{idProducto}")
+	public String visualizarProducto(@PathVariable(value = "idProducto") Long idProducto, Map<String, Object> model) {
 		Producto producto = null;
-		
-		if(idProducto > 0) {
+
+		if (idProducto > 0) {
 			producto = productoService.visualizarProducto(idProducto);
-		}else {
-			return "redirect:/listar"; 
+		} else {
+			return "redirect:/listar";
 		}
 		model.put("producto", producto);
 		model.put("titulo", "Vista Producto");
 		return "vistaProducto";
 	}
-	
+
+	@RequestMapping(value = "/eliminar/producto/{idProducto}")
+	public String eliminarProducto(@PathVariable(value = "idProducto") Long idProducto) {
+		Producto producto = null;
+
+		productoService.eliminarProductos(idProducto);
+
+		return "redirect:/listar";
+	}
+
 	//////////////////////// NO USAR /////////////////////
 
 	public String agregarFavorito() {
 		return "";
 	}
+
 	public String visualizarFavorito() {
 		return "";
 	}
