e11ad45b30eefc30f3dabea4e1047ef87b91d3ac
laki <laki@192.168.1.157>
2020-05-24 20:56:29 +0200

    Insercion de 4 usuarios nuevos con 5 producto cada usuario

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -41,3 +41,39 @@ INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,preci
 INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Unas aletas de un buen material y sin usar.','aletas.jpeg','logo.png','Cubas','Aletas profesionales',20,8,4,false);
 INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Vendo esta estanteria porque ya no puede mantener todos los libros que compro','estanteria.jpeg','logo.png','Moraleja de Enmedio','Estanteria Wollhand',60,3,4,false);
 INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Microondas en muy buen estado, aunque esta muy usado','micro.jpeg','logo.png','Serranillos del Valle','Microondas Samsung',40,13,4,false);
+
+INSERT INTO `users` (apellido, direccion, email, enabled, foto_perfil, nombre, password, telefono, username) VALUES ('Velasco','Sevilla','sara@gmail.com',1,'sara.jpg','Sara','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',657979871,'Sara');
+INSERT INTO `authorities` (user_id, authority) VALUES (5,'ROLE_USER');
+
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Aparador de los años 30','aparador.jpg','aparador2.jpg','Sevilla','Aparador de los años 30, en casi perfecto estado.',225,3,5,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Cabecero','cabecero.jpg','cabecero2.jpg','Sevilla','Cabecero gris, muy muy viejo',99,3,5,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Comoda','comoda.jpg','comoda2.jpg','Sevilla','Una comoda algo vieja pero muy espaciosa, con 4 cajones grandes',75,3,5,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Comoda de cerezo','comodacerezo.jpg','comodacerezo2.jpg','Sevilla','Una comoda de cerezo muy bien cuidada con bastantes cajones muy espaciosos',89,3,5,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Conjunto de dormitorio','dormitorio.jpg','dormitorio2.jpg','Sevilla','Una comoda con un dibujo muy bonito, rustica, Con una algo mas pequeña pero muy bonita',125,3,5,false);
+
+INSERT INTO `users` (apellido, direccion, email, enabled, foto_perfil, nombre, password, telefono, username) VALUES ('De las peñas','Humanes','david@gmail.com',1,'david.jpg','David','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',659979854,'David');
+INSERT INTO `authorities` (user_id, authority) VALUES (6,'ROLE_USER');
+
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Maquina de ejercicio en perfecto estado','maquina.webp','maquina2.webp','Humanes','Esta en perfecto estado me costo 115€ y la vendo por 60€',60,8,6,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Maquina de remo','remo.webp','remo2.webp','Humanes','Maquina de remo solo usada una vez y en perfecto estado',170,8,6,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Maquina multiuso','gimnasia.webp','gimnasia.webp','Humanes','Maquina multiuso, la vendo porque no usarla',60,8,6,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Maquina de musculacion multiestacion','musculacion.webp','musculacion.webp','Humanes','Maquina de musculacion, que tiene varias estacion de 2m de altura, 1,5m de ancho y 1,75m de largo',160,8,6,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Maquina de caminar','pedal.webp','pedal2.webp','Humanes','Maquina de caminar sin moverse, con varios tipos de resistencia',35,8,6,false);
+
+INSERT INTO `users` (apellido, direccion, email, enabled, foto_perfil, nombre, password, telefono, username) VALUES ('Herrera','Leganes','laura@gmail.com',1,'laura.jpg','Laura','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',655747216,'Laura');
+INSERT INTO `authorities` (user_id, authority) VALUES (7,'ROLE_USER');
+
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Botin de piel','botin.webp','botin2.webp','Leganes','Unos botines con poco uso de la talla 39',10,12,7,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Camiseta del gatito de piolin','camiseta.webp','camiseta.webp','Leganes','Vendo camiseta seminueva, talla M',4,12,7,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Leggins del zara','leggin.webp','leggin2.webp','Leganes','Leggins nuevos del Zara de talla S',3,12,7,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Gabardina blanca','gabardina.webp','gabardina2.webp','Leganes','Gabardina Blanca, casi sin uso de talla M/38/10',10,12,7,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Lote de ropa','lote.webp','lote2.webp','Leganes','Lote de ropa de 3 piezas, tallas M-L',20,12,7,false);
+
+INSERT INTO `users` (apellido, direccion, email, enabled, foto_perfil, nombre, password, telefono, username) VALUES ('Herrera','Leganes','laura@gmail.com',1,'javier.jpg','Javier','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',787543598,'Javier');
+INSERT INTO `authorities` (user_id, authority) VALUES (8,'ROLE_USER');
+
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Sillon de director','sillon.webp','sillon2.webp','Leganes','Sillon de director sin abrir',18,11,8,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Libro de divergente','divergente.webp','divergente2.webp','Leganes','Libro de verónica Roth de la saga que se llevó al cine con el mismo título',5,9,8,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Colección de peliculas','pelis.webp','pelis.webp','Leganes','Coleccion de peliculas para jovenes',12,9,8,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Juego de mesa','spoilers.webp','spoilers.webp','Leganes','Spoilers juego para jugar entre 2 a 10 jugadores sobtre peliculas',5,9,8,false);
+INSERT INTO producto (descripcion,foto_principal,fotos,localizacion,nombre,precio,categoria_producto_categoria_id,usuario_id,vendido) VALUES ('Blu Ray variados','lote3.webp','lote4.webp','Leganes','Blu Ray Variados, 10 Euros cada unidad bluray cine',10,9,8,false);
